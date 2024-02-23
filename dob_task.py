ce00ff3787c5367bd2f04c8b47885c9966fb4e6c
Dhammike Premachandra <dhammikerasara@gmail.com>
2024-02-23 22:34:01 +0000

    Updated

--- dob_task.py
+++ dob_task.py
@@ -1,29 +1,30 @@
 """
-Program that reads the data from the text file called DOB.txt and prints
-it out in two different sections.
-
-
+# Program that reads the data from the text file called DOB.txt and 
+# prints it out in two different sections.
+"""
 with open('DOB.txt', 'r') as file:
     """
-    #Open the file in read mode and create file object."""
+    # Open the file in read mode and create file object.
+    """
     # Read all line in the file.
-    #lines = file.readlines()
-    #name = " "
-    #birth_date = " "
-    #for line in lines:
+    lines = file.readlines()
+    name = ""
+    birth_date = ""
+    for line in lines:
         # Spilt the line into seperate indexes.
-        #name_birth_list = line.split()
-        # Extract name from the first indexes.
-        #name = name + "\n" + " ".join([name_birth_list[0], name_birth_list[1]])
+        name_birth_list = line.split()
+        # Extract name from the first two indexes.
+        name += "\n" + " ".join(name_birth_list[0:2])
         # Extract birthdate from remaining indexes.
-        #birth_date = birth_date + "\n" + " ".join(name_birth_list[2:])
+        birth_date += "\n" + " ".join(name_birth_list[2:])
     # Output the exracted name and birthdate.
-    #print(f"Name{name}\n\nBirthdate{birth_date}")
+    print(f"Name{name}\n\nBirthdate{birth_date}")
 
-    #with open('DOB.txt', 'a') as file:
+    with open('DOB.txt', 'a') as file:
+        """
         # Open the file in append mood and create file object.
+        """
         # Write name to the file.
-        #file.write(f"\nName {name}\n")
+        file.write(f"\nName{name}\n")
         # Write birthdate to the file.
-        #file.write(f"\nBirthdate {birth_date}\n")"""
-python -m pip install flake8
+        file.write(f"\nBirthdate{birth_date}\n")
